71b5e2975f1973a9192c178d1a9bbdc497e71047
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-05-28 18:32:07 +0200

    ml-kem

--- flake.nix
+++ flake.nix
@@ -1,18 +1,54 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    charon = {
+      url = "github:aeneasverif/charon";
+      inputs.nixpkgs.follows = "eurydice/nixpkgs";
+    };
+    eurydice = {
+      url = "github:aeneasverif/eurydice";
+      inputs.charon.follows = "charon";
+    };
     hax.url = "github:hacspec/hax";
-    eurydice.url = "github:aeneasverif/eurydice";
+    libcrux = {
+      url = "github:cryspen/libcrux/dev";
+      flake = false;
+    };
+    crane.follows = "charon/crane";
+    rust-overlay.follows = "charon/rust-overlay";
+    fstar.url = "github:fstarlang/fstar";
+    karamel.url = "github:fstarlang/karamel";
   };
 
   outputs =
     inputs:
     let
       system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ (import inputs.rust-overlay) ];
+      };
+      craneLib = inputs.crane.mkLib pkgs;
+      cargoLock = ./libcrux.lock;
+      src = "${inputs.libcrux}";
+      cargoArtifacts = craneLib.buildDepsOnly { inherit src cargoLock; };
     in
     {
       packages.${system} = {
         hax = inputs.hax.packages.${system}.hax;
+        charon = inputs.charon.packages.${system}.default;
         eurydice = inputs.eurydice.packages.${system}.default;
+        ml-kem = craneLib.buildPackage {
+          inherit src cargoLock cargoArtifacts;
+          name = "ml-kem";
+          buildPhase = "cd libcrux-ml-kem && bash c.sh";
+          CHARON_HOME = inputs.charon.packages.${system}.default;
+          EURYDICE_HOME =
+            pkgs.runCommand "eurydice" { }
+              "cp -r ${inputs.eurydice.packages.${system}.default}/bin $out";
+          FSTAR_HOME = inputs.fstar.packages.${system}.default;
+          KRML_HOME = inputs.karamel.packages.${system}.default.home;
+        };
       };
       inherit inputs;
     };
--- update.sh
+++ update.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 HAX_BRANCH="${HAX_BRANCH:-main}"
+CHARON_BRANCH="${CHARON_BRANCH:-main}"
 EURYDICE_BRANCH="${EURYDICE_BRANCH:-main}"
+LIBCRUX_BRANCH="${LIBCRUX_BRANCH:-dev}"
 
 # update `flake.lock`
 nix flake update \
    --override-input hax "github:hacspec/hax?ref=$HAX_BRANCH" \
-   --override-input eurydice "github:aeneasverif/eurydice?ref=$EURYDICE_BRANCH"
+   --override-input charon "github:aeneasverif/charon?ref=$CHARON_BRANCH" \
+   --override-input eurydice "github:aeneasverif/eurydice?ref=$EURYDICE_BRANCH" \
+   --override-input libcrux "github:cryspen/libcrux?ref=$LIBCRUX_BRANCH"
 
 # update `STATUS.txt`
 check () {
@@ -15,7 +19,9 @@ check () {
 }
 rm -f STATUS.txt
 check "hax" "$HAX_BRANCH"
+check "charon" "$CHARON_BRANCH"
 check "eurydice" "$EURYDICE_BRANCH"
+check "ml-kem" "$LIBCRUX_BRANCH"
 
 # commit changes
 [[ $(git diff) != "" ]] || exit 0
