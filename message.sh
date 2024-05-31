fe72b4123abd322f4d4e8de8112fd5fda112eee8
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-05-31 15:17:15 +0200

    zulip message

---
+++ message.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cat STATUS.txt | grep '❌' > /dev/null && echo '❌❌❌' || echo '✅✅✅'
+echo ""
+echo "*Links:*"
+echo "commit: https://github.com/inria-prosecco/circus-green/commit/$(git show-ref --hash refs/heads/main)"
+echo "run: https://github.com/inria-prosecco/circus-green/actions/runs/$RUN"
+echo ""
+echo "*Statuses:*"
+cat STATUS.txt
+echo ""
+echo "*Locked dependencies:*"
+cat flake.lock | jq -r '
+    .nodes |
+    .[ "nixpkgs", "fstar", "karamel", "hax", "charon", "eurydice", "libcrux" ] |
+    .locked |
+    .repo + ": https://github.com/" + .owner + "/" + .repo + "/commit/" + .rev
+    '
