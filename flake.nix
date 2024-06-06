420c3c3b06202f0c430e604ba826d93f9bc9fe7c
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-06-06 16:06:58 +0200

    move code out of this repository

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     charon = {
       url = "github:aeneasverif/charon";
       inputs.nixpkgs.follows = "eurydice/nixpkgs";
@@ -11,68 +10,32 @@
     };
     hax.url = "github:hacspec/hax";
     libcrux = {
-      url = "github:cryspen/libcrux/dev";
-      flake = false;
+      url = "github:cryspen/libcrux";
+      inputs.charon.follows = "charon";
+      inputs.eurydice.follows = "eurydice";
+      inputs.fstar.follows = "eurydice/fstar";
+      inputs.karamel.follows = "eurydice/karamel";
+      inputs.hax.follows = "hax";
     };
     bertie = {
       url = "github:cryspen/bertie";
-      flake = false;
+      inputs.hax.follows = "hax";
     };
-    crane.follows = "charon/crane";
-    rust-overlay.follows = "charon/rust-overlay";
-    fstar.url = "github:fstarlang/fstar";
-    karamel.url = "github:fstarlang/karamel";
   };
 
   outputs =
     inputs:
     let
       system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [ (import inputs.rust-overlay) ];
-      };
-      craneLib = inputs.crane.mkLib pkgs;
     in
     {
       packages.${system} = {
         hax = inputs.hax.packages.${system}.hax;
         charon = inputs.charon.packages.${system}.default;
         eurydice = inputs.eurydice.packages.${system}.default;
-        ml-kem =
-          let
-            cargoLock = ./libcrux.lock;
-            src = "${inputs.libcrux}";
-            cargoArtifacts = craneLib.buildDepsOnly { inherit src cargoLock; };
-          in
-          craneLib.buildPackage {
-            inherit src cargoLock cargoArtifacts;
-            name = "ml-kem";
-            buildPhase = "cd libcrux-ml-kem && bash c.sh";
-            CHARON_HOME = inputs.charon.packages.${system}.default;
-            EURYDICE_HOME =
-              pkgs.runCommand "eurydice" { }
-                "cp -r ${inputs.eurydice.packages.${system}.default}/bin $out";
-            FSTAR_HOME = inputs.fstar.packages.${system}.default;
-            KRML_HOME = inputs.karamel.packages.${system}.default.home;
-          };
-        bertie =
-          let
-            cargoLock = ./bertie.lock;
-            src = "${inputs.bertie}";
-            cargoArtifacts = craneLib.buildDepsOnly { inherit src cargoLock; };
-          in
-          craneLib.buildPackage {
-            inherit src cargoLock cargoArtifacts;
-            buildInputs = [
-              pkgs.python3
-              inputs.hax.packages.${system}.hax
-            ];
-            buildPhase = "python hax-driver.py extract-fstar";
-            installPhase = "cp -r . $out";
-            doCheck = false;
-          };
+        ml-kem = inputs.libcrux.packages.${system}.ml-kem;
+        bertie = inputs.bertie.packages.${system}.default;
+        inherit inputs;
       };
-      inherit inputs;
     };
 }
