c6164580e7db1718c5dbb917f9678a707e6a20fd
Nadrieril <nadrieril+git@gmail.com>
2024-12-13 15:20:33 +0100

    Remove workflow-dispatch inputs

--- update.sh
+++ update.sh
@@ -1,24 +1,7 @@
 #!/usr/bin/env bash
 
-FSTAR_BRANCH="${FSTAR_BRANCH:-master}"
-KARAMEL_BRANCH="${KARAMEL_BRANCH:-master}"
-HAX_BRANCH="${HAX_BRANCH:-main}"
-CHARON_BRANCH="${CHARON_BRANCH:-main}"
-AENEAS_BRANCH="${AENEAS_BRANCH:-main}"
-EURYDICE_BRANCH="${EURYDICE_BRANCH:-main}"
-LIBCRUX_BRANCH="${LIBCRUX_BRANCH:-main}"
-BERTIE_BRANCH="${BERTE_BRANCH:-main}"
-
 # Update `flake.lock`
-nix flake update \
-   --override-input hax "github:hacspec/hax?ref=$HAX_BRANCH" \
-   --override-input charon "github:aeneasverif/charon?ref=$CHARON_BRANCH" \
-   --override-input aeneas "github:aeneasverif/aeneas?ref=$AENEAS_BRANCH" \
-   --override-input eurydice "github:aeneasverif/eurydice?ref=$EURYDICE_BRANCH" \
-   --override-input eurydice/karamel "github:FStarLang/karamel?ref=$KARAMEL_BRANCH" \
-   --override-input eurydice/karamel/fstar "github:FStarLang/fstar?ref=$FSTAR_BRANCH" \
-   --override-input libcrux "github:cryspen/libcrux?ref=$LIBCRUX_BRANCH" \
-   --override-input bertie "github:cryspen/bertie?ref=$BERTIE_BRANCH"
+nix flake update
 
 # Update the `Cargo.lock` file we keep for libcrux
 LIBCRUX_COMMIT="$(nix shell nixpkgs#jq --command jq -r .nodes.libcrux.locked.rev flake.lock)"
