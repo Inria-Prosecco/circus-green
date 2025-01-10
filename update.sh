ae274231241b2d14474b77fa00eb206c7026e6ab
Nadrieril <nadrieril+git@gmail.com>
2025-01-10 13:04:06 +0100

    Fix `./update.sh` not updating recursive deps

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-# Update `flake.lock`
-nix flake update
+# Update `flake.lock`. `nix flake update` doesn't suffice because that won't
+# force-update nested dependencies like karamel.
+nix flake update \
+   --override-input hax "github:hacspec/hax" \
+   --override-input charon "github:aeneasverif/charon" \
+   --override-input aeneas "github:aeneasverif/aeneas" \
+   --override-input eurydice "github:aeneasverif/eurydice" \
+   --override-input eurydice/karamel "github:FStarLang/karamel" \
+   --override-input eurydice/karamel/fstar "github:FStarLang/fstar" \
+   --override-input libcrux "github:cryspen/libcrux" \
+   --override-input bertie "github:cryspen/bertie"
 
 # Update the `Cargo.lock` file we keep for libcrux
 LIBCRUX_COMMIT="$(nix shell nixpkgs#jq --command jq -r .nodes.libcrux.locked.rev flake.lock)"
