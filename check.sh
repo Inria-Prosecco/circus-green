ffa2658ef09fae38e03d2bc24b87bd924e6423ea
Nadrieril <nadrieril+git@gmail.com>
2025-04-08 14:31:15 +0200

    Update fstar and force its nixpkgs version

--- check.sh
+++ check.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-nix build -L --no-link ".#checks.$(uname -m)-linux.$1"
+# The override is a temporary hack while https://github.com/FStarLang/FStar/pull/3836 gets merged.
+nix build -L --no-link ".#checks.$(uname -m)-linux.$1" \
+    --override-input eurydice/karamel/fstar/nixpkgs "nixpkgs/88efe689298b1863db0310c0a22b3ebb4d04fbc3" \
