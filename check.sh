be4efea1e2a24b33cf3087ce25b55c8b48edd7cd
Nadrieril <nadrieril+git@gmail.com>
2025-04-09 12:30:28 +0200

    Remove manual override of fstar's nixpkgs

--- check.sh
+++ check.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# The override is a temporary hack while https://github.com/FStarLang/FStar/pull/3836 gets merged.
-nix build -L --no-link ".#checks.$(uname -m)-linux.$1" \
-    --override-input eurydice/karamel/fstar/nixpkgs "nixpkgs/88efe689298b1863db0310c0a22b3ebb4d04fbc3" \
+nix build -L --no-link ".#checks.$(uname -m)-linux.$1"
